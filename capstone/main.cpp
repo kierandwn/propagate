959472029326df6e76fce74da7a61ef70398722d
kierandwn <kieran.downie@outlook.com>
2021-01-20 22:30:35 +0000

    updated high level to read config file

--- capstone/main.cpp
+++ capstone/main.cpp
@@ -5,16 +5,15 @@
 #include "lib/dynamics/include/dynamics.h"
 #include "lib/attitude/include/matrix.h"
 
+#include "lib/config/include/config.h"
+
 using namespace std;
-
-const double kPi = 3.14159265358979323846;
-
-attitude::vector<double, 6> x0{
-    0.3, -0.4, 0.5, 1.00 * kPi / 180., 1.75 * kPi / 180., -2.20 * kPi / 180.
-};
 
 int main()
 {
-  attitude::vector<double, 6> xf = propagate::simulate(x0, 6500., .1);
+  config::init_node(
+      "C:\\Users\\kdwn\\projects\\capstone\\capstone\\config\\default.yaml");
+
+  attitude::vector<double, 6> xf = propagate::simulate();
 	return 0;
 }
